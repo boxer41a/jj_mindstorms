4c4155cde384a8d24e182a145e5bd2125401dc9b
jjj <boxer41a@yahoo.com>
2025-02-24 09:12:33 -0700

    Clean directory structure

--- includes/NXT++.h
+++ includes/NXT++.h
@@ -3,18 +3,18 @@
 // Copyright (c) 2007 by XXX
 // -------------------------------------------------------------------------
 // DESCRIPTION
-// NXT++.h is a header file containing an interface for programming Lego NXT 
-// Mindstorms. 
+// NXT++.h is a header file containing an interface for programming Lego NXT
+// Mindstorms.
 //
 // HISTORY
 // Date         By who         Description
 // -------------------------------------------------------------------------
 // 04/26/2007   maddock     OS specific includes, comment headers
-// 05/16/2007   phyizal     Removed fantom function call 
+// 05/16/2007   phyizal     Removed fantom function call
 
 // jjj  I added the defines...
-//#ifndef ___nxt_pp_h___
-//#define ___nxt_pp_h___
+#ifndef ___nxt_pp_h___
+#define ___nxt_pp_h___
 
 #ifndef ___comm_h___
    #include "comm.h"
@@ -106,12 +106,12 @@ namespace NXT
 	/*! This low-level function is used to send commands to the NXT - just like all the other functions do.
 	The only difference is that this function is harder to use, but in turn, provides much more control of the
 	NXT. For a reference about direct commands, look in the PDF titled Appendix 2. \param response Whether or not
-	you want to recieve a response from the direct command. \param dc_buf The buffer that contains the direct 
+	you want to recieve a response from the direct command. \param dc_buf The buffer that contains the direct
 	command. \param dc_buf_size The number of bytes in the direct command buffer. \param re_buf The buffer that
 	will contain the response to the direct command if specified. \param re_buf_size The number of bytes in the
 	response buffer.*/
 	void SendDirectCommand(Comm::NXTComm* comm, ViBoolean response, ViByte * dc_buf, int dc_buf_size, ViByte *re_buf, int re_buf_size);
-	
+
 	//! Contains all the functions that have to do with a motor.
 	namespace Motor
 	{
@@ -125,7 +125,7 @@ namespace NXT
 		resets the rotation to its absolute position.*/
 		void ResetRotationCount(Comm::NXTComm* comm, int port, bool relative);
 		//! Sets a motor to turn forward at a specified power.
-		/*! \param port The port that you wish to set. Can be the numbers 0-2 or OUT_A, OUT_B, or OUT_C. 
+		/*! \param port The port that you wish to set. Can be the numbers 0-2 or OUT_A, OUT_B, or OUT_C.
 		\param power The power level (0-100) that you want the motor to recieve.*/
 		void SetForward(Comm::NXTComm* comm, int port, int power);
 		//! Sets a motor to turn backward at a specified power.
@@ -149,7 +149,7 @@ namespace NXT
 		to brake or float when it is done turning.*/
 		void GoTo(Comm::NXTComm* comm, int port, int power, int tacho, bool brake);
 	}
-	
+
 	//! Contains all the functions that have to do with a sensor.
 	namespace Sensor
 	{
@@ -191,7 +191,7 @@ namespace NXT
 		int GetValue(Comm::NXTComm* comm, int port);
 		//! Retrieves multiple values of the Mindsensors Compass sensor and averages them.
 		/*! \param port The port that you wish to retrieve it's sensor's value from. Can be the numbers 0-3 or IN_1, IN_2,
-		IN_3, or IN_4. \param numValues The number of samples to average \param waitTime The time in between samples 
+		IN_3, or IN_4. \param numValues The number of samples to average \param waitTime The time in between samples
 		\return The sensor's current value. */
 		int GetAvgCmpsNxValue(Comm::NXTComm* comm, int port, int numValues, int waitTime);
 		//! Retrieves the value of the Mindsensors Compass sensor.
@@ -207,7 +207,7 @@ namespace NXT
 		IN_3, or IN_4. \return The sensor's current value. */
 		int GetCleanDistNxValue(Comm::NXTComm* comm, int port);
 		//! Gets the status of the lowspeed port.
-		/*! Most users should not need to use this. The function does not get the value of the sensor. Not to be 
+		/*! Most users should not need to use this. The function does not get the value of the sensor. Not to be
 		confused with GetSonarValue(). \param port The port that you wish to get the status of. Can be the numbers
 		0-3 or IN_1, IN_2, IN_3, or IN_4. \return The status of the specified port.*/
 		int LSGetStatus(Comm::NXTComm* comm, int port);
@@ -241,10 +241,10 @@ namespace NXT
 		ViUInt8* readI2C(Comm::NXTComm* comm, int port);
 	}
 
-    
+
 	//! NXT file functions.
 	namespace File
-	{		
+	{
 	    //! Defines different types of resource files.
 		enum FileType
 		{
@@ -254,14 +254,14 @@ namespace NXT
 			GRAPHICS, /*!< *.ric. A graphics file. */
 		    DATALOG,  /*! < *.rdt. */
 		};
-		
+
 	    //! Defines different types of file modes.
 		enum FileMode
 		{
 			READ,
 			WRITE,
 			APPEND,
-			DATA_WRITE,			
+			DATA_WRITE,
 			LINEAR_WRITE,
 		};
 
